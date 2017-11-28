686ba551563533b8a9a132a3313366955534a2fc
braunsteind <braunstein93@gmail.com>
2017-11-28 17:40:32 +0200

    Google test.

---
+++ tests/BoardTest.cpp
@@ -0,0 +1,9 @@
+//
+// Created by daniel on 28/11/17.
+//
+
+#include <gtest/gtest.h>
+
+TEST(Board, HandlesPositiveInput) {
+    EXPECT_EQ(1,1);
+}
---
+++ tests/gtest_main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by daniel on 28/11/17.
+//
+
+#include <gtest/gtest.h>
+
+GTEST_API_ int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
